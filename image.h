9ad1151b902c7d383f1c923efb64ad1042190853
Jalenques Hugo <jalenq_h@epita.fr>
2015-04-06 12:25:50 +0200

    image.h complété

--- image.h
+++ image.h
@@ -17,7 +17,7 @@
   unsigned long **integ;
 };*/
 
-
+void waitforkeypressed(void);
 
 Uint32 getpixel(SDL_Surface *surface, unsigned x, unsigned y);
 
@@ -26,6 +26,20 @@ void putpixel(SDL_Surface *surface, unsigned x, unsigned y, Uint32 pixel);
 SDL_Surface* load_image(char *path); 
 
 void init_sdl(void);
+
+void gris_normalise(SDL_Surface *img);
+
+SDL_Surface* tab_to_img(unsigned long **tab, unsigned w, unsigned h);
+
+unsigned long** rect_to_tab(struct rect *rec);
+
+void integrale(unsigned long **integ, unsigned w, unsigned h);
+
+unsigned long** img_to_tab(SDL_Surface *img);
+
+unsigned long** e_to_24(unsigned long **array, unsigned w, unsigned h);
+
+unsigned long** adaboost_rec(struct rect *rec);
 
 #endif
 
