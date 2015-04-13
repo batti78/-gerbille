85934fa4bdc3ac4a2eef5311ba755617aff4b94e
Jalenques Hugo <jalenq_h@epita.fr>
2015-04-13 18:22:47 +0200

    images enlevées

--- Makefile
+++ Makefile
@@ -7,16 +7,13 @@ LDFLAGS=
 LDLIBS= `pkg-config --libs sdl` -lSDL_image
 SRC= main.c
 SRC2= main2.c
-SRC3= main3.c
 OBJ= ${SRC:.c=.o}
 OBJ2= ${SRC2:.c=.o}
-OBJ3= ${SRC3:.c=.o}
-all: main3
+all: main
 main: ${OBJ}
 main2: ${OBJ2}
-main3: ${OBJ3}
 clean:
 	rm -f *~ *.o
-	rm -f main main2 main3
+	rm -f main main2
 		       
 			     # END
--- main2.c
+++ main2.c
@@ -4,19 +4,33 @@
 #include<SDL_image.h>
 #include<SDL/SDL.h>
 #include"image.c"
+#include<dirent.h>
+#include<sys/types.h>
 
 int main(int argc, char *argv[])
 {
   if(argc == 2)
   {
-    char *path = argv[1]; 
+    char *path = argv[1];
+
+    DIR *rep = NULL;
+    struct dirent *fichier = NULL;
+    rep = opendir(path);
+    printf("open\n");
+    if (rep == NULL)
+      printf("Dossier inexistant");
+    fichier = readdir(rep);
+    printf("read");
+    path = "des.png";
+    printf("given\n");
+    
     init_sdl();
     SDL_Surface *img = load_image(path);
     gris_normalise(img);
     unsigned long **tab = img_to_tab(img);
     
     //integrale(tab, img->w, img->h);
-    //
+
     struct rect *rec = malloc(sizeof(struct rect));
     rec->x = 200;
     rec->y = 200;
@@ -28,10 +42,18 @@ int main(int argc, char *argv[])
     unsigned long **test = e_to_24(tab, img->w, img->h);
     display_image(img);
     display_image(tab_to_img(test, 24, 24));
-    
     display_image(tab_to_img(ada, 24, 24));
-
+    for (unsigned i = 0; i < 24; i++)
+      free(test[i]);
+    printf("1\n");
+    for (unsigned j = 0; j < 24; j++)
+      free(ada[j]);
+    printf("2\n");
     free(rec);
+
+    if (closedir(rep) == -1)
+      printf("Erreur fermeture");
+
     return 0; 
    }
 }
