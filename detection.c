a5de96d8948be5befd12400c108a1ec3978076d3
Jalenques Hugo <jalenq_h@epita.fr>
2015-06-08 18:47:11 +0200

    detection squelette

--- detection.c
+++ detection.c
@@ -1,4 +1,20 @@
 #include<stdlib.h>
 #include<stdio.h>
 
-
+void parcours(unsigned long **array, unsigned w, unsigned h)
+{
+  for (unsigned i = 0; i < w; i++)
+  {
+    for (unsigned j = 0; j < h; j++)
+    {
+      for (unsigned i1 = 24; i1 < w - i; i1++)
+      {
+        for (unsigned j1 = 24; j1 < h - j; j1++)
+        {
+          // est-ce que le carré de dim i1xj1 est un visage ?
+          // si oui dessine un carré rouge sinon rien
+        }
+      }
+    }
+  }
+}
