fc8ff521e6d8f2e1659eb670f13934bb2d0e2415
Jalenques Hugo <jalenq_h@epita.fr>
2015-06-08 19:46:37 +0200

    meilleur carré

--- detection.c
+++ detection.c
@@ -9,13 +9,22 @@ void parcours(unsigned long **array, unsigned w, unsigned h)
       for (unsigned i1 = 24; i1 < w - i; i1++)
         for (unsigned j1 = 24; j1 < h - j; j1++)
         {
-          // intégralisation
           // si oui dessine un carré noir sinon rien
           if (visage)
           {
+            array[i-1][j-1] = array[i-1][j1] = array[i1][j-1] = array[i1][j1] = 255;
             for (unsigned i2 = i; i2 < i1; i2++)
               for (unsigned j2 = j; j2 < j1; j2++)
-                array[i2][j2] = 255;
+              {
+                if (i2 == i)
+                  array[i2 - 1][j2] = 255;
+                if (i2 == i1 - 1)
+                  array[i2 + 1][j2] = 255;
+                if (j2 == j)
+                  array[i2][j2 - 1] = 255;
+                if (j2 == j1 - 1)
+                  array[i2][j2 + 1] = 255;
+              }
           }
         }
 }
